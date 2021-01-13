64d44e58119bada11c0aa7a0eacf415c0d58c2bc
trbarrettjr <trbarrettjr@users.noreply.github.com>
2021-01-13 11:41:49 -0800

    Create meeting-mute.py

---
+++ meeting-mute.py
@@ -0,0 +1,20 @@
+import usb_hid
+from adafruit_hid.keyboard import Keyboard
+from adafruit_hid.keyboard_layout_us import KeyboardLayoutUS
+from adafruit_hid.keycode import Keycode
+from adafruit_circuitplayground import cp
+
+kbd = Keyboard(usb_hid.devices)
+layout = KeyboardLayoutUS(kbd)
+
+while True:
+    while cp.button_a and cp.switch: # For Zoom; Audio Toggle; Switch Left
+        kbd.press(Keycode.GUI, Keycode.SHIFT, Keycode.A)
+    while cp.button_b and cp.switch: # For Zoom; Video Toggle; Switch Left
+        kbd.press(Keycode.GUI, Keycode.SHIFT, Keycode.V)
+    while cp.button_a and not cp.switch: # For Google Meet; Audio Toggle; Switch Right
+        kbd.press(Keycode.GUI, Keycode.D)
+    while cp.button_b and not cp.switch: # For Google Meet; Video Toggle; Switch Right
+        kbd.press(Keycode.GUI, Keycode.E)
+    else:
+        kbd.release_all()
