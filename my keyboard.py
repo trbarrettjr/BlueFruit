fe03ce828c1f451287269b7c7b4a3d5f6a1329c6
trbarrettjr <trbarrettjr@users.noreply.github.com>
2021-04-10 11:18:47 -0700

    Fixed Typos

--- my keyboard.py
+++ my keyboard.py
@@ -18,24 +18,24 @@ button_b_pressed = False
 
 while True:
     if cp.switch:
-        if cp.button_a and button_a_pressed is False:
+        if cp.button_a and not button_a_pressed:
             kbd.send(Keycode.GUI, Keycode.SHIFT, Keycode.A)
             button_a_pressed = cp.button_a
-        elif cp.button_a is False and button_a_pressed is True:
+        elif not cp.button_a and button_a_pressed:
             button_a_pressed = cp.button_a
-        if cp.button_b and button_b_pressed is False:
+        if cp.button_b and not button_b_pressed:
             kbd.send(Keycode.GUI, Keycode.SHIFT, Keycode.V)
             button_b_pressed = cp.button_b
-        elif cp.button_b is False and button_b_pressed is True:
+        elif not cp.button_b and button_b_pressed:
             button_b_pressed = cp.button_b
     else:
-        if cp.button_a and button_a_pressed is False:
+        if cp.button_a and not button_a_pressed:
             kbd.send(Keycode.GUI, Keycode.D)
             button_a_pressed = cp.button_a
-        elif cp.button_a if False and button_a_pressed is True:
+        elif not cp.button_a and not button_a_pressed:
             button_a_pressed = cp.button_a
-        if cp.button_b and button_b_pressed is False:
+        if cp.button_b and not button_b_pressed:
             kbd.send(Keycode.GUI, Keycode.E)
             button_b_pressed = cp.button_b
-        elif cp.button_b is False and button_b_pressed is True:
+        elif not cp.button_b and button_b_pressed:
             button_b_pressed = cp.button_b
